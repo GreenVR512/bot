d7e152d3a5863fcbc96c720b88086f3d61b5d989
GreenVR512 <168782926+GreenVR512@users.noreply.github.com>
2025-04-09 17:47:04 -0700

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,9 +1,1 @@
-import discord
-from discord.ext import commands
-import os
-from dotenv import load_dotenv
 
-load_dotenv()  # Load variables from .env
-
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-bot.run(os.getenv("MTM1OTMxNTgyNzI3NDQyMDMzNg.GWC-16.PPvjzRDu5gk8oGhzpfBPAWbvoU7_9sBZIICa1c"))
